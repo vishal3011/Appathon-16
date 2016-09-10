826805331f84fb31369973272c8924e554803a3f
RajuKoushik <g.rajukoushik@gmail.com>
2016-09-11 04:29:43 +0530

    Updated Home and Drawer.

--- app/src/main/java/com/android/example/appathon/ActivityUserProfile.java
+++ app/src/main/java/com/android/example/appathon/ActivityUserProfile.java
@@ -9,6 +9,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,11 +25,16 @@ import java.util.Map;
 public class ActivityUserProfile extends AppCompatActivity {
     private TextView textView;
     private FloatingActionButton buttonFabPost;
+    private ArrayAdapter mAdapter;
     private ListView mDrawerList;
     private DrawerLayout mDrawerLayout;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapterRecycler;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ActionBarDrawerToggle mDrawerToggle;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
@@ -38,8 +44,12 @@ public class ActivityUserProfile extends AppCompatActivity {
 
         textView.setText("Welcome User " + intent.getStringExtra(LoginActivity.KEY_USERNAME));
 
-
         mDrawerList = (ListView) findViewById(R.id.navList);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+
+        addDrawerItems();
+        setupDrawer();
 
 
         buttonFabPost = (FloatingActionButton) findViewById(R.id.fab_post);
@@ -54,7 +64,42 @@ public class ActivityUserProfile extends AppCompatActivity {
     }
 
 
+    private void addDrawerItems() {
+        Log.i("MainActivity:", "Setting up drawer");
+        String[] cats = {"Women Rights", "Safety & Security","Health","Gender Equality","Education","Miscellaneous"};
 
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cats);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle("TheFemiApp");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
 
 
 
