9499ef7f9f5c7c2aacb8217b92bb6df4e9ce1d73
RajuKoushik <g.rajukoushik@gmail.com>
2016-09-11 03:54:05 +0530

    Updated Home and Drawer.

--- app/src/main/java/com/android/example/appathon/ActivityUserProfile.java
+++ app/src/main/java/com/android/example/appathon/ActivityUserProfile.java
@@ -5,17 +5,27 @@ package com.android.example.appathon;
  */
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.Map;
 
 public class ActivityUserProfile extends AppCompatActivity {
     private TextView textView;
     private FloatingActionButton buttonFabPost;
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +38,10 @@ public class ActivityUserProfile extends AppCompatActivity {
 
         textView.setText("Welcome User " + intent.getStringExtra(LoginActivity.KEY_USERNAME));
 
+
+        mDrawerList = (ListView) findViewById(R.id.navList);
+
+
         buttonFabPost = (FloatingActionButton) findViewById(R.id.fab_post);
 
         buttonFabPost.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +52,8 @@ public class ActivityUserProfile extends AppCompatActivity {
             }
         });
     }
+
+
 
 
 
