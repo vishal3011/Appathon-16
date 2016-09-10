ad3651bf07c43960778132d65c720d58655a878f
vishal3011 <thinkdifferent.vshal@gmail.com>
2016-09-11 02:54:20 +0530

    Added Floating Point Button and Edit post activities

--- app/src/main/java/com/android/example/appathon/ActivityUserProfile.java
+++ app/src/main/java/com/android/example/appathon/ActivityUserProfile.java
@@ -4,14 +4,18 @@ package com.android.example.appathon;
  * Created by Vishal on 10-09-2016.
  */
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ActivityUserProfile extends AppCompatActivity {
     private TextView textView;
+    private FloatingActionButton buttonFabPost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +27,19 @@ public class ActivityUserProfile extends AppCompatActivity {
         Intent intent = getIntent();
 
         textView.setText("Welcome User " + intent.getStringExtra(LoginActivity.KEY_USERNAME));
+
+        buttonFabPost = (FloatingActionButton) findViewById(R.id.fab_post);
+
+        buttonFabPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(ActivityUserProfile.this,EditPostActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+
 
 
 }
---
+++ app/src/main/java/com/android/example/appathon/EditPostActivity.java
@@ -0,0 +1,51 @@
+package com.android.example.appathon;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+/**
+ * Created by Vishal on 11-09-2016.
+ */
+public class EditPostActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private static final String[]categories = {"item 1", "item 2", "item 3"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_edit_post);
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(EditPostActivity.this,
+                android.R.layout.simple_spinner_item,categories);
+
+
+        spinner.setAdapter(adapter);
+
+
+
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+
+        }
+    }
+
+}
